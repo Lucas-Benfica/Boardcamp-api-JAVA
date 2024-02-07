004395ad0b5439d7ffe423238c6147c7cfd5dde7
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-07 19:28:42 -0300

    fix: add @Data

--- src/main/java/com/boardcamp/api/dtos/GamesDTO.java
+++ src/main/java/com/boardcamp/api/dtos/GamesDTO.java
@@ -3,7 +3,9 @@ package com.boardcamp.api.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
+@Data
 public class GamesDTO {
     @NotBlank(message = "Name is required.")
     private String name;
--- src/main/java/com/boardcamp/api/dtos/RentalsDTO.java
+++ src/main/java/com/boardcamp/api/dtos/RentalsDTO.java
@@ -2,7 +2,9 @@ package com.boardcamp.api.dtos;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
+@Data
 public class RentalsDTO {
 
     @NotNull(message = "Price per day total is required.")
