ed8987d592f20ddd06e5c9ff6ceb0890b1c2c0bd
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-07 16:04:10 -0300

    feat: Models

--- src/main/java/com/boardcamp/api/models/GamesModel.java
+++ src/main/java/com/boardcamp/api/models/GamesModel.java
@@ -1,5 +1,18 @@
 package com.boardcamp.api.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GamesModel {
     
+    private Long id;
+    private String name;
+    private String image;
+    private Long stockTotal;
+    private Long pricePerDay;
+
 }
--- src/main/java/com/boardcamp/api/models/RentalsModel.java
+++ src/main/java/com/boardcamp/api/models/RentalsModel.java
@@ -1,5 +1,21 @@
 package com.boardcamp.api.models;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RentalsModel {
-    
+    private Long id;
+    private Long customerId;
+    private Long gameId;
+    private LocalDate rentDate;
+    private Long daysRented;
+    private LocalDate returnDate;
+    private Long originalPrice;
+    private Long delayFee;
 }
