ee4c6224cd70ee582c95df1ec2a637210700698b
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-08 23:17:31 -0300

    fix: find rentals query

--- src/main/java/com/boardcamp/api/repositories/RentalsRepository.java
+++ src/main/java/com/boardcamp/api/repositories/RentalsRepository.java
@@ -3,6 +3,8 @@ package com.boardcamp.api.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.boardcamp.api.models.RentalsModel;
@@ -10,6 +12,9 @@ import com.boardcamp.api.models.RentalsModel;
 @Repository
 public interface RentalsRepository extends JpaRepository<RentalsModel, Long> {
 
-    List<RentalsModel> findAllByGameId(Long gameId);
+    
+    @Query(value = "SELECT * FROM rentals WHERE rentals.game_id = :gameId AND rentals.return_date IS NULL", nativeQuery = true)
+    List<RentalsModel> findAllByGameId(@Param("gameId") Long gameId);
     
 }
+//List<RentalsModel> findAllByGameId(Long gameId);
